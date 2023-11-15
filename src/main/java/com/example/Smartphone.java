f904e3a802e795d4fb91068f485ea202447aac0d
Kaoru-kubota-Karura <cosmo.kaoru1211@gmail.com>
2023-11-15 10:25:06 +0800

    ver.β

--- src/main/java/com/example/Smartphone.java
+++ src/main/java/com/example/Smartphone.java
@@ -4,15 +4,16 @@ package com.example;
 
 public class Smartphone extends ElectronicDevice{
     // TODO: Declare the necessary attributes (brand, model, batteryLevel)
-    private String brand;
-    private String model;
+    // private String brand;
+    // private String model;
     private int batteryLevel;
 
     // Constructor to initialize the Smartphone object
     public Smartphone(String brand, String model, int batteryLevel) {
         // TODO: Initialize the attributes with the provided parameters
-        this. brand = brand;
-        this. model = model;
+        // this. brand = brand;
+        // this. model = model;
+        super(brand, model);
         this. batteryLevel = batteryLevel;
     }
 
@@ -27,7 +28,14 @@ public class Smartphone extends ElectronicDevice{
     // Implement the checkBattery method to return:
         // "Warning: Battery low!" if the battery level is 20% or lower.
         // "Battery level is okay: [batteryLevel]%." if the battery level is above 20%.
-    if (batteryLevel<20)
+    public void testBatteryCheck(){
+        if (batteryLevel<20){
+            System.out.println("Warning: Battery low!"); 
+        }
+        else{
+            System.out.println("Battery level is okay:");
+        }
+    }
 
     // TODO: Implement necessary getters for the attributes
 
