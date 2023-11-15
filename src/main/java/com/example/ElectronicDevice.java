15ed812c9366daa24c54331629e7d2f636c78dcf
Kaoru-kubota-Karura <cosmo.kaoru1211@gmail.com>
2023-11-15 10:18:47 +0800

    ver.β

--- src/main/java/com/example/ElectronicDevice.java
+++ src/main/java/com/example/ElectronicDevice.java
@@ -2,12 +2,29 @@ package com.example;
 
 public class ElectronicDevice {
     // TODO: Declare common attributes for electronic devices, such as brand and model.
+        private String brand;
+        private String model; 
 
     // Constructor to initialize an ElectronicDevice object
     public ElectronicDevice(String brand, String model) {
         // TODO: Initialize the common attributes (brand, model) with the provided parameters
+        // private String brand;
+        // private String model; 
+        this.brand = brand;
+        this.model = model;
     }
-
+    public String getBrand(){
+        return this. brand;
+    }
+    public String getModel(){
+        return this. model;
+    }
+    public void setBrand (String brand){
+        this.brand = brand;
+    }
+    public void setModel (String model){
+        this.model = model;
+    }
     // TODO: Implement getters (and setters if necessary) for the brand and model.
 
     // OPTIONALLY implement the required methods to pass the BonusTest.
--- src/main/java/com/example/Smartphone.java
+++ src/main/java/com/example/Smartphone.java
@@ -2,18 +2,32 @@ package com.example;
 
 // TODO: make this class extend ElectronicDevice
 
-public class Smartphone {
+public class Smartphone extends ElectronicDevice{
     // TODO: Declare the necessary attributes (brand, model, batteryLevel)
+    private String brand;
+    private String model;
+    private int batteryLevel;
 
     // Constructor to initialize the Smartphone object
     public Smartphone(String brand, String model, int batteryLevel) {
         // TODO: Initialize the attributes with the provided parameters
+        this. brand = brand;
+        this. model = model;
+        this. batteryLevel = batteryLevel;
+    }
+
+    public int getBatteryLevel(){
+        return this. batteryLevel;
+    }
+    public void setBrand (int batteryLevel){
+        this.batteryLevel = batteryLevel;
     }
 
     // TODO: Implement a method to check the battery level and return a message based on the level
     // Implement the checkBattery method to return:
         // "Warning: Battery low!" if the battery level is 20% or lower.
         // "Battery level is okay: [batteryLevel]%." if the battery level is above 20%.
+    if (batteryLevel<20)
 
     // TODO: Implement necessary getters for the attributes
 
